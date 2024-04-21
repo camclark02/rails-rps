a5822b567de66ab9baea801f2ae568588da01a1a
camclark02 <164952709+camclark02@users.noreply.github.com>
2024-04-21 18:57:35 +0000

    created homepage without ref links

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,6 @@
+class ZebraController < ApplicationController
+  def home_page
+    render({:template => "game_templates/home"})
+  end
+  def 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+get("/", {:controller =>"zebra" , :action => "home_page"})
+
+get("/rock", {:controller =>"zebra" , :action => "rock_page"})
+
+get("/paper", {:controller =>"zebra" , :action => "paper_page"})
+
+get("/scissors", {:controller =>"zebra" , :action => "scissors_page"})
+
 
 end
