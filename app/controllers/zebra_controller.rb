d07af64dcbf94c6e70773f566b9237eabae5cf64
camclark02 <164952709+camclark02@users.noreply.github.com>
2024-04-21 19:06:20 +0000

    done with rock

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,6 +1,34 @@
 class ZebraController < ApplicationController
   def home_page
-    render({:template => "game_templates/home"})
+    render({ :template => "game_templates/home" })
   end
-  def 
+
+  def rock_page
+    @moves = ["rock", "paper", "scissors"]
+
+    @comp_move = @moves.sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    render({ :template => "game_templates/rock_page" })
+  end
+
+  def paper_page
+    moves = ["rock", "paper", "scissors"]
+
+    @comp_move = moves.sample
+
+    if @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+  end
 end
